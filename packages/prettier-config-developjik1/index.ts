25f1da838fb423394ad8de4308a46d9f10d943bb
정인권 <developjik@colosseum.kr>
2025-06-23 13:22:17 +0900

    feat(prettier-config): add Prettier configuration and package setup

---
+++ packages/prettier-config-developjik1/index.ts
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+    semi: true, // 세미콜론 사용 여부
+    tabWidth: 2, // 탭 너비
+    singleQuote: true, // 작은따옴표 사용 여부
+    printWidth: 100, // 한 줄의 최대 길이
+    trailingComma: 'all', // 후행 콤마 사용 방식
+    arrowParens: 'always', // 화살표 함수 괄호 사용 방식
+}; 
